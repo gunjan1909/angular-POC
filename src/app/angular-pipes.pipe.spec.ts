5403a3e4e28e237e9e6e62bb5c45e6b0c2d203a5
gunjan1909 <bhanarkaranita@gmail.com>
2024-04-13 17:42:33 +0530

    testing files removed gitignore updated

--- src/app/angular-pipes.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AngularPipesPipe } from './angular-pipes.pipe';
-
-describe('AngularPipesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AngularPipesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
